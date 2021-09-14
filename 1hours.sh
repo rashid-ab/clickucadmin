541472f49e3f2cb0cb34a811bb163c9c83e6ab32
unknown <Rashidbuttuog@gmail.com>
2021-09-14 07:24:59 -0700

    ForgetPassword

--- 1hours.sh
+++ 1hours.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/php /home/u595034023/domains/punjabpolicenews.com/public_html/ScratchPubg/cronJobs/cronjob_1_hour.php cron:run > /dev/null 2>&1
+/usr/bin/php /home/u109931872/domains/rabdesignstories.com/public_html/clickuc/cronJobs/cronjob_1_hour.php cron:run > /dev/null 2>&1
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -129,7 +129,7 @@ class DashboardController extends Controller
         $headers = array();
         $headers[] = 'Content-Type: application/json';
 
-        $headers[] = 'Authorization: key= AAAA_i3zE_w:APA91bHLc6FS-w_ZJLiD6l4ga6DDALcmh23ShgGKuW4TstHsjmNaiypaolSBhktLt2xmC77jL_bqJOIj6SCs9W5Uk7AwqR_ndByT7IYH70bQfmBxuzV5Vdgp5iEUa7HgfuF9cuxa1g8r';
+        $headers[] = 'AAAAuAma-80:APA91bEm160o7dHP9wsiHf67ntA9D6l6v9b--CCNnHuDR2zSC-KypYnAP-Xph1PDQFbDeFD-yPw65O4vzx2lSDUoE08YReRRu1xdU4L6jpqvJ_DhbQ7Mg-KfxS8t5xiwCNs1ymzxrSpO';
 
         //Setup curl, add headers and post parameters.
 
