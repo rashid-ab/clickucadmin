215720a1de0c1b231249ab09ec5fb1b086e5075c
unknown <Rashidbuttuog@gmail.com>
2021-09-14 08:08:29 -0700

    ForgetPassword

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -290,6 +290,7 @@ class ApiController extends Controller {
         /*======================  Redeem  =====================*/
 
         public function redeem(Request $request){
+            echo $request->user_id;
             $user=auth('api')->user();
             if(!$user){
                 return response()->json(['status' => "200",
